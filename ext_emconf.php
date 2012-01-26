6d2798c614b8ebfe1dcae0340181d2c23a4cd24f
m.u.guenther <m.u.guenther@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-01-26 16:06:09 +0000

    reset the _md5_values_when_last_written in ext_emconf.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tcemainhooks/trunk@56772 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "tcemainhooks".
 #
-# Auto generated 26-01-2012 12:16
+# Auto generated 26-01-2012 16:44
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -39,7 +39,7 @@ This extension provides a simple interface that informs you on those changes. It
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"0448";s:25:"class.tx_tcemainhooks.php";s:4:"8c83";s:30:"class.tx_tcemainhooks_test.php";s:4:"4c19";s:36:"class.tx_tcemainhooks_user_class.php";s:4:"893d";s:16:"ext_autoload.php";s:4:"286e";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"d39c";s:44:"interface.tx_tcemainhooks_user_interface.php";s:4:"798b";s:14:"doc/manual.sxw";s:4:"3647";s:19:"doc/wizard_form.dat";s:4:"f3e4";s:20:"doc/wizard_form.html";s:4:"c814";}',
+	'_md5_values_when_last_written' => 'a:10:{s:25:"class.tx_tcemainhooks.php";s:4:"8c83";s:30:"class.tx_tcemainhooks_test.php";s:4:"4c19";s:36:"class.tx_tcemainhooks_user_class.php";s:4:"893d";s:16:"ext_autoload.php";s:4:"286e";s:12:"ext_icon.gif";s:4:"aa6e";s:17:"ext_localconf.php";s:4:"d39c";s:44:"interface.tx_tcemainhooks_user_interface.php";s:4:"798b";s:14:"doc/manual.sxw";s:4:"3f49";s:19:"doc/wizard_form.dat";s:4:"f3e4";s:20:"doc/wizard_form.html";s:4:"c814";}',
 	'suggests' => array(
 	),
 );
