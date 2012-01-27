3df3c91963068fd4e6cb71d85efa2fe93ddd93e5
michi.k <michi.k@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-01-27 11:58:41 +0000

    Updated ext-emconf
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tcemainhooks/trunk@56816 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "tcemainhooks".
 #
-# Auto generated 26-01-2012 16:44
+# Auto generated 27-01-2012 12:55
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -30,7 +30,7 @@ This extension provides a simple interface that informs you on those changes. It
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.0',
+	'version' => '1.0.1',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -39,7 +39,7 @@ This extension provides a simple interface that informs you on those changes. It
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:25:"class.tx_tcemainhooks.php";s:4:"8c83";s:30:"class.tx_tcemainhooks_test.php";s:4:"4c19";s:36:"class.tx_tcemainhooks_user_class.php";s:4:"893d";s:16:"ext_autoload.php";s:4:"286e";s:12:"ext_icon.gif";s:4:"aa6e";s:17:"ext_localconf.php";s:4:"d39c";s:44:"interface.tx_tcemainhooks_user_interface.php";s:4:"798b";s:14:"doc/manual.sxw";s:4:"3f49";s:19:"doc/wizard_form.dat";s:4:"f3e4";s:20:"doc/wizard_form.html";s:4:"c814";}',
+	'_md5_values_when_last_written' => 'a:8:{s:25:"class.tx_tcemainhooks.php";s:4:"7478";s:30:"class.tx_tcemainhooks_test.php";s:4:"4c19";s:36:"class.tx_tcemainhooks_user_class.php";s:4:"bffe";s:16:"ext_autoload.php";s:4:"286e";s:12:"ext_icon.gif";s:4:"aa6e";s:17:"ext_localconf.php";s:4:"d39c";s:44:"interface.tx_tcemainhooks_user_interface.php";s:4:"6a6c";s:14:"doc/manual.sxw";s:4:"6458";}',
 	'suggests' => array(
 	),
 );
